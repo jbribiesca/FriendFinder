8f6e2c9c4ee415ecb6aff09ee375a94447ac6126
jbribiesca <bribiesca@gmail.com>
2019-04-30 15:25:18 -0500

    fixed routes and server.js

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+Friends = [
+    {
+        name: "Jerry",
+        photo: "http://c317203.r3.cf1.rackcdn.com/mugshots2_jerry.jpg",
+        scores: ["1", "2"]
+    }
+]
+
+
+module.exports = Friends;
