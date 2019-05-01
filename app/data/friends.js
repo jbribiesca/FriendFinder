db2feb18883a87e4364c2a2964527d3ef73f3d63
jbribiesca <bribiesca@gmail.com>
2019-05-01 14:19:40 -0500

    make it better

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,66 @@ Friends = [
     {
         name: "Jerry",
         photo: "http://c317203.r3.cf1.rackcdn.com/mugshots2_jerry.jpg",
-        scores: ["1"]
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        name: "Ben Affleck",
+        photo: "http://www.gstatic.com/tv/thumb/persons/7530/7530_v9_bb.jpg",
+        scores: [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        name: "Ryan Reynolds",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Deadpool_2_Japan_Premiere_Red_Carpet_Ryan_Reynolds_%28cropped%29.jpg/220px-Deadpool_2_Japan_Premiere_Red_Carpet_Ryan_Reynolds_%28cropped%29.jpg",
+        scores: [
+            4,
+            3,
+            2,
+            5,
+            3,
+            1,
+            4,
+            2,
+            4,
+            5
+        ]
+    },
+    {
+        name: "Ryan Gosling",
+        photo: "https://m.media-amazon.com/images/M/MV5BMTQzMjkwNTQ2OF5BMl5BanBnXkFtZTgwNTQ4MTQ4MTE@._V1_UY317_CR18,0,214,317_AL_.jpg",
+        scores: [
+            3,
+            2,
+            1,
+            5,
+            4,
+            5,
+            4,
+            2,
+            4,
+            5
+        ]
     }
 ]
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,7 +7,30 @@ module.exports = function (app) {
     });
 
     app.post("/api/friends", function (req, res) {
-        friendData.push(req.body);
-        res.json(req.body)
+
+        var bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: 1000
+        }
+
+        var newFriend = req.body
+        var newFriendScores = newFriend.scores
+        var totalDifference = 0
+
+        for (i = 0; i < friendData.length; i++){
+            totalDifference = 0
+            for (var j=0; j< friendData[i].scores[j]; j++){
+                totalDifference += Math.abs(parseInt(newFriendScores[j]) - parseInt(friendData[i].scores[j]));
+                if (totalDifference <= bestMatch.friendDifference){
+					bestMatch.name = friendData[i].name;
+                    bestMatch.photo = friendData[i].photo;
+                    bestMatch.friendDifference = totalDifference;
+				}
+            }
+        }
+
+        friendData.push(newFriend);
+        res.json(bestMatch)
     });
 }
